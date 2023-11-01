38abdb15b888382a840dbf32134ccfb3625b8b40
zebin-dm <zebin.cai@deepmirror.com>
2023-11-01 17:40:23 +0800

    update

--- tools/launch_model_worker.sh
+++ tools/launch_model_worker.sh
@@ -1,4 +1,4 @@
 set -e
-source ./set_env.sh
+source ./tools/set_env.sh
 model_path=/mnt/nas/share-map/common/models/llm/llava/llava-v1.5-13b
-python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path ${model_path} --load-4bit
+python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path ${model_path} --load-4bit > output/worker.log 2>&1 &
